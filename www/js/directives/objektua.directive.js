2e11c96d4d836ba7af0a4283c9aa035052319c0d
Iñigo Alza Osés <ialza@iametza.eus>
2017-01-17 09:00:24 +0100

    eszenaren objektuak eszenatokira mugatu

--- www/js/directives/objektua.directive.js
+++ www/js/directives/objektua.directive.js
@@ -9,32 +9,56 @@ app.directive ('objektua', ['$cordovaDialogs', 'Database', function ($cordovaDia
           initAngle = attrs.rotate !== undefined ? attrs.rotate : 0,
           rotationInit = 0,
           transform = {  translate :{ x: attrs.x, y: attrs.y   }, scale: initScale, angle: initAngle, rx: 0, ry: 0, rz: 0 },
-          offsetX = 0, offsetY = 0;
+          offset = {'x': 0, 'y': 0},
+          limits = {'top': 0, 'right': 0, 'bottom': 0, 'left': 0},
+          eszenatokia = angular.element ('#eszenatokia');
+          
+      limits.left = eszenatokia[0].offsetLeft + 15;
+      limits.top = eszenatokia[0].offsetTop + 15;
+      limits.right = eszenatokia[0].offsetWidth - 15;
+      limits.bottom = eszenatokia[0].offsetHeight - 15;
       
       element.children ().attr ('src', attrs.background);
+      
+      // Le damos "id" al elemento para poder hacer una txapuzilla luego
+      element.children ().attr ('id', 'objektua_' + element.attr ('data-eo-id'));
       
       if (attrs.scale === undefined)
         element.children ().css ({ transform: 'translate3d(' + attrs.x + 'px, ' + attrs.y + 'px, 0)'});
       
-      var updateElementTransform = function (dbGorde){
+      var updateElementTransform = function (transform_new, dbGorde){
         dbGorde = typeof dbGorde !== 'undefined' ? dbGorde : false;
         
-        var value = 'translate3d(' + transform.translate.x + 'px, ' + transform.translate.y + 'px, 0) ' +
-                    'scale(' + transform.scale + ', ' + transform.scale + ') ' +
-                    'rotate('+  transform.angle + 'deg)';
-                    
-        var css = { 'transform': value, '-webkit-transform': value, '-moz-transform': value, '-o-transform': value };
+        element.children ().css (transform2css (transform_new));
         
-        element.children ().css (css);
-        
-        if (dbGorde){
-          var id = parseInt (element.attr ('data-eo-id'));
-          var style = JSON.stringify (element[0].children[0].style);
-      
-          Database.query ('UPDATE eszena_objektuak SET style=? WHERE id=?', [style, id]).then (function (){}, function (error){
-            console.log ("Objektua directive UPDATE eszena_objektuak", error);
-          });
+        var bounds = document.getElementById ("objektua_" + element.attr ('data-eo-id')).getBoundingClientRect ();
+        if (bounds.top < limits.top || bounds.bottom > limits.bottom || bounds.right > limits.right || bounds.left < limits.left){
+          element.children ().css (transform2css (transform));
+          
+          return (false);
         }
+        else{
+          transform = transform_new;
+        
+          if (dbGorde){
+            var id = parseInt (element.attr ('data-eo-id'));
+            var style = JSON.stringify (element[0].children[0].style);
+        
+            Database.query ('UPDATE eszena_objektuak SET style=? WHERE id=?', [style, id]).then (function (){}, function (error){
+              console.log ("Objektua directive UPDATE eszena_objektuak", error);
+            });
+          }
+          
+          return (true);
+        }
+      };
+      
+      var transform2css = function (t){
+        var value = 'translate3d(' + t.translate.x + 'px, ' + t.translate.y + 'px, 0) ' +
+                    'scale(' + t.scale + ', ' + t.scale + ') ' +
+                    'rotate('+  t.angle + 'deg)';
+                    
+        return ({ 'transform': value, '-webkit-transform': value, '-moz-transform': value, '-o-transform': value });
       };
       
       scope.onPress = function (){
@@ -72,64 +96,86 @@ app.directive ('objektua', ['$cordovaDialogs', 'Database', function ($cordovaDia
       scope.onRotate = function (event){
         
         if (event.target === element[0].children[0]){
-          transform.angle = parseFloat (initAngle) + parseFloat (event.rotation - rotationInit);
-          transform.rz = 1;
-          updateElementTransform ();
+          
+          var t = {  translate :{ x: transform.translate.x, y: transform.translate.y   }, scale: transform.scale, angle: transform.angle, rx: 0, ry: 0, rz: 0 };
+          
+          t.angle = parseFloat (initAngle) + parseFloat (event.rotation - rotationInit);
+          
+          updateElementTransform (t);
+          
         }
         
       };
       
       scope.onRotateEnd = function (){
         
-        initAngle = transform.angle;
-        updateElementTransform (true);
+        if (updateElementTransform (transform, true))
+          initAngle = transform.angle;
         
       };
       
       scope.onPinch = function (event){
         
         if (event.target === element[0].children[0]){
-          transform.scale = initScale * event.scale;
-          updateElementTransform();
+          
+          var t = {  translate :{ x: transform.translate.x, y: transform.translate.y   }, scale: transform.scale, angle: transform.angle, rx: 0, ry: 0, rz: 0 };
+          
+          t.scale = initScale * event.scale;
+          
+          updateElementTransform (t);
+          
         }
         
       };
       
       scope.onPinchEnd = function (){
         
-        initScale = transform.scale;
-        updateElementTransform (true);
+        if (updateElementTransform (transform, true))
+          initScale = transform.scale;
         
       };
       
       scope.onPanStart = function (event){
         
-        offsetX = event.srcEvent.offsetX;
-        offsetY = event.srcEvent.offsetY;
+        offset.x = event.srcEvent.offsetX;
+        offset.y = event.srcEvent.offsetY;
         
       };
       
       scope.onPan = function (event){
         
         if (event.target === element[0].children[0]){
-          transform.translate.x = event.center.x - offsetX;
-          transform.translate.y = event.center.y - offsetY;
-          updateElementTransform ();
+          
+          var t = {  translate :{ x: transform.translate.x, y: transform.translate.y   }, scale: transform.scale, angle: transform.angle, rx: 0, ry: 0, rz: 0 };
+          
+          t.translate.x = event.center.x - offset.x;
+          t.translate.y = event.center.y - offset.y;
+          
+          updateElementTransform (t);
+          
         }
         
       };
       
       scope.onPanEnd = function (){
         
-        updateElementTransform (true);
+        updateElementTransform (transform, true);
         
       };
       
       scope.onDblClick = function (){
         
-        initAngle = transform.angle = 0;
-        initScale = transform.scale = transform.rz = 1;
-        updateElementTransform (true);
+        var t = {  translate :{ x: transform.translate.x, y: transform.translate.y   }, scale: transform.scale, angle: transform.angle, rx: 0, ry: 0, rz: 0 };
+        
+        t.angle = 0;
+        t.scale = 1;
+        
+        if (updateElementTransform (t, true)){
+          
+          initAngle = 0;
+          initScale = 1;
+          
+        }
         
       };
       
