e33e01f061bdc5b343a465c969d561ca1d2515ad
Julen Irazoki <julen@iametza.eus>
2017-03-26 18:30:39 +0200

    musika berria, menuak gorde, play stop eta atzera, fondoak aldatu

--- www/js/controllers/erabiltzaileak.controller.js
+++ www/js/controllers/erabiltzaileak.controller.js
@@ -1,38 +1,38 @@
 app.controller('ErabiltzaileakCtrl',['$scope', 'Database', '$uibModal', function($scope, Database, $uibModal){
-  
+
   $scope.erabiltzaileak = [];
-  
+
   $scope.init = function (){
-    
+
     $scope.soinuak.audio_fondo_play ('sarrera');
-    
-    angular.element ('#eszenatokia').css ('background', "url('images/fondoa.jpg') no-repeat center center fixed");
+
+    angular.element ('#eszenatokia').css ('background', "url('images/defektuzko-fondoa.png') no-repeat center center fixed");
     angular.element ('#eszenatokia').css ('background-size', "cover");
-    
+
     // Recogemos los erabiltzaileak
     $scope.getErabiltzaileak ();
-    
+
   };
-  
+
   $scope.getErabiltzaileak = function (){
-    
+
     Database.getRows ('erabiltzaileak', '', ' ORDER BY izena ASC').then (function (emaitza){
-      
+
       $scope.erabiltzaileak = emaitza;
-      
+
       angular.forEach (emaitza, function (erab, ind){
         if (erab.argazkia.trim () != '')
           $scope.erabiltzaileak[ind].argazkia = cordova.file.dataDirectory + erab.argazkia;
       });
-      
+
     }, function (error){
       console.log ("ErabiltzaileakCtrl, getErabiltzaileak", error);
     });
-    
+
   };
-  
+
   $scope.erabiltzailea_datuak = function (erabiltzailea_id){
-    
+
     var modala = $uibModal.open ({
       animation: true,
       backdrop: 'static',
@@ -44,26 +44,26 @@ app.controller('ErabiltzaileakCtrl',['$scope', 'Database', '$uibModal', function
         }
       }
     });
-    
+
     modala.rendered.then (function (){
       $scope.soinuak.audio_play ('popup');
     });
-    
+
     modala.result.then (function (){
-                
+
       // Recogemos los erabiltzaileak
       $scope.getErabiltzaileak ();
-      
+
     }, function (error){
       console.log ("ErabiltzaileakCtrl, erabiltzailea_datuak modala", error);
     });
-    
+
   };
-  
+
   document.addEventListener ('deviceready', function (){
-    
+
     $scope.init ();
-    
+
   });
 
 }]);
--- www/js/controllers/ipuinak.controller.js
+++ www/js/controllers/ipuinak.controller.js
@@ -1,69 +1,69 @@
 app.controller('IpuinakCtrl',['$scope', '$route', 'Database', 'Funtzioak', '$uibModal', function($scope, $route, Database, Funtzioak, $uibModal){
-  
+
   $scope.erabiltzailea = {};
   $scope.ipuinak = [];
-  
+
   $scope.init = function () {
-    
+
     $scope.soinuak.audio_fondo_play ('sarrera');
-    
-    angular.element ('#eszenatokia').css ('background', "url('images/fondoa.jpg') no-repeat center center fixed");
+
+    angular.element ('#eszenatokia').css ('background', "url('images/defektuzko-fondoa.png') no-repeat center center fixed");
     angular.element ('#eszenatokia').css ('background-size', "cover");
-    
+
     // Recogemos los datos del erabiltzaile
     Database.getRows ('erabiltzaileak', {'id': $route.current.params.erabiltzailea_id}, '').then (function (emaitza){
-      
+
       if (emaitza.length === 1){
         $scope.erabiltzailea = emaitza[0];
-        
+
         // Recogemos los ipuinak del erabiltzaile
         $scope.getIpuinak ();
       }
       else
         window.location = "#/";
-      
+
     }, function (error){
       console.log ("IpuinakCtrl, erabiltzaile datuak jasotzen", error);
     });
-    
+
   };
-  
+
   $scope.getIpuinak = function (){
     var ipuinak = [];
-    
+
     Database.query ("SELECT ipuinak.*, ifnull(irudiak.cordova_file, '') cordova_file, ifnull(irudiak.path, '') path, ifnull(irudiak.izena, '') izena FROM ipuinak LEFT JOIN eszenak ON eszenak.fk_ipuina=ipuinak.id LEFT JOIN irudiak ON irudiak.id=eszenak.fk_fondoa AND irudiak.atala='fondoa' WHERE ipuinak.fk_erabiltzailea=? ORDER BY ipuinak.izenburua ASC, eszenak.orden ASC", [$scope.erabiltzailea.id]).then (function (emaitza){
-      
+
       angular.forEach (emaitza, function (ipuina){
-        
+
         if ((pos = $scope.ipuina_pos (ipuinak, ipuina.id)) < 0)
           ipuinak.push ({'id': ipuina.id, 'izenburua': ipuina.izenburua, 'fullPath': Funtzioak.get_fullPath (ipuina)});
         else if (ipuinak[pos].fullPath.trim () === '')
           ipuinak[pos].fullPath = Funtzioak.get_fullPath (ipuina);
-          
+
       });
-      
+
       $scope.ipuinak = ipuinak;
-      
+
     }, function (error){
       console.log ("IpuinakCtrl, getIpuinak", error);
     });
-    
-    
+
+
   };
-  
+
   $scope.ipuina_pos = function (lista, id){
-    
+
     for (var i = 0; i < lista.length; i++){
       if (lista[i].id == id)
         return (i);
     }
-    
+
     return (-1);
-    
+
   };
-  
+
   $scope.ipuina_datuak = function (ipuina_id){
-    
+
     var modala = $uibModal.open ({
       animation: true,
       backdrop: 'static',
@@ -78,26 +78,26 @@ app.controller('IpuinakCtrl',['$scope', '$route', 'Database', 'Funtzioak', '$uib
         }
       }
     });
-    
+
     modala.rendered.then (function (){
       $scope.soinuak.audio_play ('popup');
     });
-    
+
     modala.result.then (function (){
-                
+
       // Recogemos los ipuinak del erabiltzaile
       $scope.getIpuinak ();
-      
+
     }, function (error){
       console.log ("IpuinakCtrl, ipuina_datuak modala", error);
     });
-    
+
   };
-  
+
   document.addEventListener ('deviceready', function (){
-    
+
     $scope.init ();
-    
+
   });
 
 }]);
